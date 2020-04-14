cf451f06d3aaad2096a0af3cd8a74ce635dc8158
Maria Luque Anguita <mluqueanguita@gmail.com>
2020-04-14 11:19:09 +0200

    made it into a class

---
+++ country.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 14 09:28:59 2020
+
+@author: Maria
+"""
+
+class Sector(object):
+    def __init__(self, name):
+        self.name = name
+        self.percent = ''
+        self.startDate = ''
+        self.endDate = ''
+        
+    def getSector(self):
+        sector = [self.name, self.percent, self.startDate, self.endDate]
+        return sector
+    
+
+class CountryData(object):
+    def __init__(self, name):
+        
+        self.name = name
+        self.sectors = []
+        
+    def add_sector(self, sector):
+        self.sectors.append(Sector(sector))
+        
+        
+      
+    
+    
--- runProcess.py
+++ runProcess.py
@@ -13,6 +13,7 @@ import os
 import glob
 import csv
 from PyPDF2 import PdfFileReader, PdfFileWriter
+from country import CountryData, Sector
 
 # split pages1 and 2 from every PDF
 def pdf_splitter(path):
@@ -37,25 +38,38 @@ def merger(output_path, input_paths):
         pdf_writer.write(fh)
 
 # Get important values for each part (retail, grocery...)
-def getValues(string):
+def getValues(short):
+    # Name
+    sector = Sector(short[0][0])
+    
     percentages = []
     dates = []
-    values = ['']
     months = ['Jan ', 'Feb ', 'Mar ', 'Apr ', 'May ', 'Jun ',
               'Jul ', 'Aug ', 'Sep ', 'Oct ', 'Nov ', 'Dec ']
-    for x in string:
+    for x in short:
         for i in x:
             if '%' in i:
                 percentages.append(i)
             if any(word in i for word in months):
                 dates.append(i)
     try:
-        values.append(string[0][0]) # Name
-        values.append(percentages[2]) # Percentage in the middle ignores the 40%s and 80%s
-        values.append(dates[0]) # First date from graph
-        values.append(dates[2]) # Last date from graph
-    except: print('...')
-    finally: return values
+        # Dates
+        sector.startDate = dates[0]
+        sector.endDate = dates[2]
+        
+        # Percentages
+        percent_set = set(percentages)
+        if len(percent_set) == 5: # if percentage is not 40% or 80%
+            percentage = [ x for x in percent_set if "40" not in x and '80' not in x][0]
+        else: # if percentage is either 40 or 80
+            if percentages.count('+80%') == 2: percentage = '+80%'
+            if percentages.count('-80%') == 2: percentage = '-80%'
+            if percentages.count('+40%') == 2: percentage = '+40%'
+            if percentages.count('-40%') == 2: percentage = '-40%'
+        sector.percent = percentage
+            
+    except: print('')
+    finally: return sector
     
 def checkEmptyDates(final):
     try:
@@ -77,56 +91,84 @@ def checkEmptyDates(final):
                 final2.append(a)
     finally: return final2
 
-
+def readCSV(path):
+    dataset = []
+    with open(path, encoding="utf8") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            dataset.append(row)
+    return dataset
 
 if __name__ == '__main__':
-    # split first 2 pages of each country from folder gdata2020-04-10
-    # Change name of folder if PDFs are in another folder
-    paths = glob.glob('gdata2020-04-10/*.pdf')
-    for path in paths:
-        pdf_splitter(path)
-
-    # merge page 1 and 2 of each country and output them to folder PDFs
-    files = glob.glob('output/*.pdf') 
-    for page in range(0, len(files), 2):
-        paths = [files[page], files[page+1]]
-        name = "PDFs/" + files[page][18:35] +".pdf"
-        print(name, paths) 
-        merger(name, paths)
-    print("Success!")
     
+#    # split first 2 pages of each country from folder gdata2020-04-10
+#    # Change name of folder if PDFs are in another folder
+#    paths = glob.glob('gdata2020-04-10/*.pdf')
+#    for path in paths:
+#        pdf_splitter(path)
+#
+#    # merge page 1 and 2 of each country and output them to folder PDFs
+#    files = glob.glob('output/*.pdf') 
+#    for page in range(0, len(files), 2):
+#        paths = [files[page], files[page+1]]
+#        name = "PDFs/" + files[page][18:35] +".pdf"
+#        print(name, paths) 
+#        merger(name, paths)
+#    print("Success!")
+#    
     
     
 
     # Read CSV files from folder CSVs
-    paths = glob.glob('CSVs/*.csv')
+    paths = glob.glob('CSVs/AE*.csv')
     final = []
     for path in paths:
-        dataset = []
-        with open(path, encoding="utf8") as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            for row in csv_reader:
-                dataset.append(row)
-
+        
+        dataset = readCSV(path)
+        
         # Read only valuable information
         titles = ['Retail', 'Grocery', 'Parks', 'Transit', 'Work', 'Residential']
-        final.append([dataset[1][0]])
+        
+        country = CountryData(dataset[1][0])
+        
         i = 0
         while i < len(dataset):
             if len(dataset[i]) != 0:
                 if any(word in dataset[i][0] for word in titles):
                     short = [[dataset[i][0]]]
-                    for x in range(1, 10):
+                    for x in range(1, 12): # for the next 12 lines check if the section ends
                         try:
-                            if any(word in dataset[i+x][0] for word in titles): break
+                            #when sector is complete
+                            if any(word in dataset[i+x][0] for word in titles): 
+                                sector = getValues(short)
+                                country.add_sector(sector)
+                                break
+                            # keep adding info to sector until complete
                             else:
                                 short.append(dataset[i+x])
-                        except: print("...")
-                    final.append(getValues(short))
+                        except: 
+                            if short[0][0] == 'Residential': 
+                                sector = getValues(short)
+                                if len(country.sectors) == 5:
+                                    country.add_sector(sector)
+                            print('...')
             i += 1
+        
+        print(country.name)
+        for s in country.sectors:
+            print(s.name.getSector())
             
+        final.append([country.name])
+        for sector in country.sectors:
+            last = ['']
+            for i in sector.name.getSector(): last.append(i)
+            final.append(last)
+        
+        print("\n--END OF CSV FILE--\n")
+        
+        
     # Check for empty dates and correct
-    final = checkEmptyDates(final)
+#    final = checkEmptyDates(final)
 
     # Save info to a file
     with open('final.csv', 'w', newline='') as file:
