720fd9609cc7c267c6ab7bfbdcf35e14e56f2a65
Maria Luque Anguita <mluqueanguita@gmail.com>
2020-05-11 14:47:20 +0200

    need to do it for each day

--- stringency/countryConvert.py
+++ stringency/countryConvert.py
@@ -249,11 +249,21 @@ COUNTRY_ALPHA3_TO_COUNTRY_ALPHA2 = {
     'ZWE': 'ZW',
 }
 
+ISO2_to_ISO3 = {'AB': 'ABH', 'AW': 'ABW', 'AF': 'AFG', 'AO': 'AGO', 'AI': 'AIA', 'AX': 'ALA', 'AL': 'ALB', 'AD': 'AND', 'AE': 'UAE', 'AR': 'ARG', 'AM': 'ARM', 'AS': 'ASM', 'AG': 'ATG', 'AU': 'AUS', 'AT': 'AUT', 'AZ': 'AZE', 'BI': 'BDI', 'BE': 'BEL', 'BJ': 'BEN', 'BF': 'BFA', 'BD': 'BGD', 'BG': 'BGR', 'BH': 'BHR', 'BS': 'BHS', 'BA': 'BIH', 'BL': 'BLM', 'BY': 'BLR', 'BZ': 'BLZ', 'BM': 'BMU', 'BO': 'BOL', 'BR': 'BRA', 'BB': 'BRB', 'BN': 'BRN', 'BT': 'BTN', 'BV': 'BVT', 'BW': 'BWA', 'CF': 'CAF', 'CA': 'CAN', 'CC': 'CCK', 'CH': 'CHE', 'CL': 'CHL', 'CN': 'CHN', 'CI': 'CIV', 'CM': 'CMR', 'CD': 'COD', 'CG': 'COG', 'CK': 'COK', 'CO': 'COL', 'KM': 'COM', 'CV': 'CPV', 'CR': 'CRI', 'CU': 'CUB', 'CW': 'CUW', 'CX': 'CXR', 'KY': 'CYM', 'CY': 'CYP', 'CZ': 'CZE', 'DE': 'DEU', 'DJ': 'DJI', 'DM': 'DMA', 'DK': 'DNK', 'DO': 'DOM', 'DZ': 'DZA', 'EC': 'ECU', 'EG': 'EGY', 'ER': 'ERI', 'ES': 'ESP', 'EE': 'EST', 'ET': 'ETH', 'FI': 'FIN', 'FJ': 'FJI', 'FK': 'FLK', 'FR': 'FRA', 'FO': 'FRO', 'FM': 'FSM', 'GA': 'GAB', 'GB': 'GBR', 'GE': 'GEO', 'GG': 'GGY', 'GH': 'GHA', 'GI': 'GIB', 'GN': 'GIN', 'GP': 'GLP', 'GM': 'GMB', 'GW': 'GNB', 'GQ': 'GNQ', 'GR': 'GRC', 'GD': 'GRD', 'GL': 'GRL', 'GT': 'GTM', 'GF': 'GUF', 'GU': 'GUM', 'GY': 'GUY', 'HK': 'HKG', 'HM': 'HMD', 'HN': 'HND', 'HR': 'HRV', 'HT': 'HTI', 'HU': 'HUN', 'ID': 'IDN', 'IM': 'IMN', 'IN': 'IND', 'IO': 'IOT', 'IE': 'IRL', 'IR': 'IRN', 'IQ': 'IRQ', 'IS': 'ISL', 'IL': 'ISR', 'IT': 'ITA', 'JM': 'JAM', 'JE': 'JEY', 'JO': 'JOR', 'JP': 'JPN', 'KZ': 'KAZ', 'KE': 'KEN', 'KG': 'KGZ', 'KH': 'KHM', 'KI': 'KIR', 'KN': 'KNA', 'KR': 'KOR', 'KW': 'KWT', 'LA': 'LAO', 'LB': 'LBN', 'LR': 'LBR', 'LY': 'LBY', 'LC': 'LCA', 'LI': 'LIE', 'LK': 'LKA', 'LS': 'LSO', 'LT': 'LTU', 'LU': 'LUX', 'LV': 'LVA', 'MF': 'MAF', 'MA': 'MAR', 'MC': 'MCO', 'MD': 'MDA', 'MG': 'MDG', 'MV': 'MDV', 'MX': 'MEX', 'MH': 'MHL', 'MK': 'MKD', 'ML': 'MLI', 'MT': 'MLT', 'MM': 'MMR', 'ME': 'MNE', 'MN': 'MNG', 'MP': 'MNP', 'MZ': 'MOZ', 'MR': 'MRT', 'MS': 'MSR', 'MQ': 'MTQ', 'MU': 'MUS', 'MW': 'MWI', 'MY': 'MYS', 'YT': 'MYT', 'NA': 'NAM', 'NC': 'NCL', 'NE': 'NER', 'NF': 'NFK', 'NG': 'NGA', 'NI': 'NIC', 'NU': 'NIU', 'NL': 'NLD', 'NO': 'NOR', 'NP': 'NPL', 'NR': 'NRU', 'NZ': 'NZL', 'OM': 'OMN', 'OS': 'OST', 'PK': 'PAK', 'PA': 'PAN', 'PE': 'PER', 'PH': 'PHL', 'PW': 'PLW', 'PG': 'PNG', 'PL': 'POL', 'PR': 'PRI', 'KP': 'PRK', 'PT': 'PRT', 'PY': 'PRY', 'PS': 'PSE', 'PF': 'PYF', 'QA': 'QAT', 'RE': 'REU', 'RO': 'ROU', 'RU': 'RUS', 'RW': 'RWA', 'SA': 'SAU', 'SD': 'SDN', 'SN': 'SEN', 'SG': 'SGP', 'GS': 'SGS', 'SH': 'SHN', 'SB': 'SLB', 'SL': 'SLE', 'SV': 'SLV', 'SM': 'SMR', 'SO': 'SOM', 'PM': 'SPM', 'RS': 'SRB', 'SS': 'SSD', 'ST': 'STP', 'SR': 'SUR', 'SK': 'SVK', 'SI': 'SVN', 'SE': 'SWE', 'SZ': 'SWZ', 'SC': 'SYC', 'SY': 'SYR', 'TC': 'TCA', 'TD': 'TCD', 'TG': 'TGO', 'TH': 'THA', 'TJ': 'TJK', 'TK': 'TKL', 'TM': 'TKM', 'TO': 'TON', 'TT': 'TTO', 'TN': 'TUN', 'TR': 'TUR', 'TV': 'TUV', 'TW': 'TWN', 'TZ': 'TZA', 'UG': 'UGA', 'UA': 'UKR', 'UY': 'URY', 'US': 'USA', 'UZ': 'UZB', 'VC': 'VCT', 'VE': 'VEN', 'VG': 'VGB', 'VI': 'VIR', 'VN': 'VNM', 'VU': 'VUT', 'WF': 'WLF', 'WS': 'WSM', 'YE': 'YEM', 'ZA': 'ZAF', 'ZM': 'ZMB', 'ZW': 'ZWE'}
 
-def convert_country_alpha3_to_country_alpha2(country_3_code):
+
+def iso3_to_2(country_3_code):
     """Convert country code to country name.
     """
     if country_3_code not in COUNTRY_ALPHA3_TO_COUNTRY_ALPHA2:
         raise KeyError
 
     return COUNTRY_ALPHA3_TO_COUNTRY_ALPHA2[country_3_code]
+
+def iso2_to_3(country_2_code):
+    """Convert country code to country name.
+    """
+    if country_2_code not in ISO2_to_ISO3:
+        return ('country 2 code not in list')
+
+    return ISO2_to_ISO3[country_2_code]
--- stringency/stringencyData.py
+++ stringency/stringencyData.py
@@ -7,36 +7,65 @@ Created on Mon May 11 11:58:14 2020
 
 from urllib.request import urlopen
 import json
-from countryConvert import convert_country_alpha3_to_country_alpha2
+from countryConvert import iso2_to_3
+import pandas as pd
+import csv
+from datetime import date
 
 url_start = 'https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/date-range/'
 
-# date1 = input('Enter start date in the format YYYY-MM-DD:')
-# date2 = input('Enter end date in the format YYYY-MM-DD:')
+date1 = input('Enter start date in the format YYYY-MM-DD:')
+date2 = input('Enter end date in the format YYYY-MM-DD:')
 
-date1 = '2020-02-02'
-date2 = '2020-02-03'
+# date1 = '2020-02-02'
+# date2 = '2020-02-03'
 
 url_final = url_start + date1 + '/' + date2
 
 def save_file(filename, data, extension):
-    fileN = filename + '.' + extension
-    with open(fileN, 'w') as outfile:
-        json.dump(data, outfile)
+    if extension == 'json':
+        with open(filename +'.json', 'w') as outfile:
+            json.dump(data, outfile)
+    if extension == 'csv':
+        with open(filename + '.csv', 'w') as outfile:
+            for key in data.keys():
+                outfile.write("%s,%s\n"%(key,data[key]))
+
+def check_exists(data, date1, country):
+    try:
+        hello = data['data'][date1][iso2_to_3(country)]['stringency_actual']
+        return True
+    except: return False
+
+def count_days(date1, date2):
+    f_date = date(int(date1[:4]), int(date1[5:7]), int(date1[8:]))
+    l_date = date(int(date2[:4]), int(date2[5:7]), int(date2[8:]))
+    delta = l_date - f_date
+    print(delta.days)
+
+def load_data():
+    try:
+        json_url = urlopen(url_final)
+        data = json.loads(json_url.read())
+        # save_file('oxford_values', data, 'json')
+        xls = pd.ExcelFile('emissions_reduction_master_sheet.xlsx')
+        df1 = pd.read_excel(xls, 'modal_trans_countries_covered')
+        countries_covered = df1.iloc[1:130, 0]
+        return countries_covered, data
+    except Exception as e: print(e)
+    return None
 
 
-
-try:
-    json_url = urlopen(url_final)
-    data = json.loads(json_url.read())
-    # print(data)
-    save_file('oxford_values', data, 'json')
+countries_covered, data = load_data()  
+stringency_country = {}
+for country in countries_covered:
+    if check_exists(data, date1, country) == True:
+        stringency_country[country] = data['data'][date1][iso2_to_3(country)]['stringency_actual']
+        print(country, data['data'][date1][iso2_to_3(country)]['stringency_actual'])
+    else: 
+        stringency_country[country] = 'nan'
     
-    print(data['data']['2020-02-02']['AFG']['date_value'])
-    print()
-    print(data['data']['2020-02-02']['AFG'])
-    print([convert_country_alpha3_to_country_alpha2('AFG')])
-    print(data['data']['2020-02-02'][convert_country_alpha3_to_country_alpha2('AFG')])
-    
-except Exception as e: print(e)
+save_file('stringency_country', stringency_country, 'csv')
+
+
 
